cdeb9eb32d30ef9c0d58b98568c98c3a770a693d
Daniil <Daniil@DESKTOP-8A3V6T7>
2022-07-26 18:09:53 +0700

    Add project files.

---
+++ PhoneBook.Domain/Person.cs
@@ -0,0 +1,10 @@
+﻿using PhoneBook.Domain.Basics;
+
+namespace PhoneBook.Domain
+{
+    public class Person : Identity
+    {
+        public string Name { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+    }
+}
