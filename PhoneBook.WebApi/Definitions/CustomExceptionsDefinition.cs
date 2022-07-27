89fd1fadddd3b1aedb02541d154c314287b42d3b
D A <st076580@student.spbu.ru>
2022-07-27 13:46:04 +0700

    Middleware Exception Handler

---
+++ PhoneBook.WebApi/Definitions/CustomExceptionsDefinition.cs
@@ -0,0 +1,13 @@
+﻿using PhoneBook.WebApi.Definitions.Base;
+using PhoneBook.WebApi.Middleware;
+
+namespace PhoneBook.WebApi.Definitions
+{
+    public class CustomExceptionsDefinition : AppDefinition
+    {
+        public override void ConfigureApplication(WebApplication app, IWebHostEnvironment environment)
+        {
+            app.UseMiddleware<CustomExceptionHandlerMiddleware>();
+        }
+    }
+}
